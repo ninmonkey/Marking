bccfc31d31bbc6ce4a98b0f435b89f0d040ae59e
Jake Bolton <ninmonkeys@gmail.com>
2025-12-13 15:18:21 -0600

    clean: remove extra workspace, add build ( ref #1 )

---
+++ Build/marking.Build.Module.ps1
@@ -0,0 +1,91 @@
+﻿<#
+.EXAMPLE
+    . .\Build\marking.Build.Module.ps1; $commands_summary ;
+#>
+$myFile       = $MyInvocation.MyCommand.ScriptBlock.File
+$myModuleName = 'marking'
+$myRoot       = $myFile | Split-Path | Split-Path
+$BuildConfig = @{
+    LineEnding = "`r`n"
+}
+
+Push-Location -Stack 'marking.build' $myRoot
+$commands_public   = @(
+    # to recurse or not ?
+    @( foreach ($potentialDirectory in 'Commands') {
+        Join-Path $myRoot $potentialDirectory | Get-ChildItem -ea ignore -Recurse
+    })
+    | Where-Object name -NotMatch '^Scrap'
+    | ? Extension -in '.ps1' #, '.psm1', '.psd1'
+)
+
+[Collections.Generic.List[object]] $commands_summary = @()
+$commands_summary.AddRange(
+    @(
+        $commands_public
+        | %{
+            $item = $_
+            [pscustomobject]@{
+                PSTypeName    = 'build.marking.command.public'
+                Public        = $true
+                Name          = $Item.Name
+                Size          = '{0:n2} kb' -f ( $Item.Length / 1kb )
+                LastWriteTime = $Item.LastWriteTime
+                # Path          = $Item
+                FullName      = $Item                                  # convert to alias
+                # Documentation = ''
+                # HasRequiresStatment        = $false
+                # HasUsingNamespaceStatement = $false
+            }
+        }
+    )
+)
+
+$destinationRoot = $myRoot
+
+$commands_summary
+    | Join-String -f "`n {0}" -op 'Commands' -p {
+        [System.IO.Path]::GetRelativePath( $myRoot, $_.FullName )
+    }
+    | Write-Host -fg 'magenta'
+Pop-Location -Stack 'marking.build'
+
+if( $commands_summary.count -gt 0 ) {
+    $myModuleFile = Join-Path $DestinationRoot "${myModuleName}.psm1"
+
+    @(
+        # todo: optimize IO. And minimize any extra memory allocations for strings
+        foreach ( $item in $commands_summary )  {
+            ( Get-Content -raw (Get-Item $item.FullName ) ) -replace '\r?\n', $BuildConfig.LineEnding
+        }
+    )
+    | Join-String -sep $BuildConfig.LineEnding
+    | Set-Content -Path $MyModuleFile -encoding UTF8 -ProgressAction Continue # -Confirm
+}
+return
+if ($commands_public) {
+    $myFormatFile = Join-Path $destinationRoot "$myModuleName.format.ps1xml"
+    $commands_public
+        | Out-FormatData -Module $MyModuleName
+        | Set-Content $myFormatFile -Encoding UTF8 -Verbose -ProgressAction Continue # -Confirm
+    Get-Item $myFormatFile
+}
+
+$types = @(
+    # Add your own Write-TypeView statements here
+    # or declare them in the 'Types' directory
+    Join-Path $myRoot Types
+        | Get-Item -ea ignore
+        | Import-TypeView
+
+)
+
+if ($types) {
+    $myTypesFile = Join-Path $destinationRoot "$myModuleName.types.ps1xml"
+    $types
+        | Out-TypeData
+        | Set-Content $myTypesFile -Encoding UTF8 -Verbose -ProgressAction Continue # -Confirm
+
+    Get-Item $myTypesFile
+}
+Pop-Location
--- Marking.psd1
+++ Marking.psd1
@@ -8,7 +8,7 @@
     RootModule        = 'marking.psm1'
 
     # Version number of this module.
-    ModuleVersion     = '0.0.4'
+    ModuleVersion     = '0.0.1'
 
     # Supported PSEditions
     # CompatiblePSEditions = @()
