6b1725a4b2876ef5b9b3eb1aee0186c9cc9aba4f
Jake Bolton <ninmonkeys@gmail.com>
2026-09-15 11:09:28 -0500

    create Write-MdHeader and test

---
+++ Commands/Public/Write-MdHeader.ps1
@@ -0,0 +1,13 @@
+﻿function Write-MdHeader {
+    <#
+    .SYNOPSIS
+        Writes Md headers, like <h1>, <h2>, etc.
+    #>
+    [CmdletBinding()]
+    param(
+        [string] $Text,
+        [Alias('Depth')] [int] $Level = 1
+    )
+    $prefix = '#' * $Level -join ''
+    " ${Prefix} ${Text}"
+}
---
+++ Tests/Commands/Public/Write-MdHeader.Tests.ps1
@@ -0,0 +1,19 @@
+﻿Describe 'Write-MdHeader' {
+    Context 'Contains exact response' {
+        it 'Text: <Text>, Level: <Level>' -ForEach @(
+            @{
+                Text = 'Title'
+                Level = 1
+                Expected = ' # Title'
+            }
+            @{
+                Text = 'Name'
+                Level = 3
+                Expected = ' ### Name'
+            }
+        ) {
+            $actual = Write-MdHeader -Text $Text -Level $Level
+            $actual | Should-BeString -Expected $Expected
+        }
+    }
+}
