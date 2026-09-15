75bd9694d2d7a522a2f3bf7abae7bfa33eaeb8d6
Jake Bolton <ninmonkeys@gmail.com>
2026-09-15 11:17:21 -0500

    add: reload script

---
+++ reload.ps1
@@ -0,0 +1,9 @@
+﻿<#
+.SYNOPSIS
+    Unload, Rebuild, and Import module as global
+#>
+Remove-module Marking* -ea ignore
+
+$error.clear()
+. (Join-Path $PSScriptRoot './build.ps1' )
+Import-Module -Scope Global -force -Passthru (Join-Path $PSScriptRoot './Marking.psd1' )
