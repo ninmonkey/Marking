67dbe4eeca1028ce12b07aad5f902650df73ba41
Jake Bolton <ninmonkeys@gmail.com>
2026-09-15 11:53:30 -0500

    build: Marking v0.0.4

--- Marking.psm1
+++ Marking.psm1
@@ -1,6 +1,6 @@
 <#
 .Description
-    Module built on: 2026-09-15 11:23:55Z
+    Module built on: 2026-09-15 11:52:24Z
 #>
 
 #region Module.Before.ps1
@@ -24,11 +24,44 @@ function Write-MdHeader {
     #>
     [CmdletBinding()]
     param(
+        # Header text
         [string] $Text,
-        [Alias('Depth')] [int] $Level = 1
+
+        # Header depth. The number of '#'. ( Default: 1 )
+        [ArgumentCompletions(1, 2, 3, 4, 5, 6)]
+        [Alias('Depth')]
+        [int] $Level = 1
     )
-    $prefix = '#' * $Level -join ''
-    "${Prefix} ${Text}"
+    end {
+        $prefix = '#' * $Level -join ''
+        "${Prefix} ${Text}"
+    }
+}
+
+function Write-MdList {
+    <#
+    .SYNOPSIS
+        Writes Md lists, - item 1
+    #>
+    [CmdletBinding()]
+    param(
+        # Header text
+        [Parameter(ValueFromPipeline)]
+        [Alias('InputObject', 'Content')]
+        [object[]] $Text
+    )
+    begin {
+        [Text.StringBuilder] $sb  = ''
+        $ListIcon = '-'
+    }
+    process {
+        foreach( $Item in $Text ) {
+            [void] $sb.AppendFormat("{0} {1}`n", $ListIcon, $Item)
+        }
+    }
+    end {
+        return $sb.ToString()
+    }
 }
 
 
