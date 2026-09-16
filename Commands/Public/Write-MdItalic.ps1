c91c5b5ed4e7d4da51582baef4e1615fbb4cf91b
Jake Bolton <ninmonkeys@gmail.com>
2026-09-16 12:01:27 -0500

    create: Write-MdItalic and tests

---
+++ Commands/Public/Write-MdItalic.ps1
@@ -0,0 +1,19 @@
+﻿function Write-MdItalic {
+    <#
+    .SYNOPSIS
+        Writes Italic text
+    .example
+        'foo bar' | Write-MdItalic
+        # *foo bar*
+    #>
+    [OutputType( [string] )]
+    [CmdletBinding()]
+    param(
+        # Header text
+        [Parameter(ValueFromPipeline)]
+        [string] $Text
+    )
+    process {
+        '*{0}*' -f $Text
+    }
+}
---
+++ Tests/Commands/Public/Write-MdItalic.Tests.ps1
@@ -0,0 +1,23 @@
+﻿Describe 'Write-MdItalic' {
+    Context 'Bold: <Text>' -ForEach @(
+        @{
+            Text = 'File List:'
+            Expected = '*File List:*'
+            | NormalizeLineEnding
+        }
+    ) {
+        It 'By Param Name is: <Expected>' {
+            $actual = Write-MdItalic -Text $Text | NormalizeLineEnding
+            $actual | Should-BeString -Expected $Expected # -TrimWhitespace # -NormalizeLineEnding
+        }
+        It 'By Param Pos is: <Expected>' {
+            $actual = Write-MdItalic $Text | NormalizeLineEnding
+            $actual | Should-BeString -Expected $Expected # -TrimWhitespace # -NormalizeLineEnding
+        }
+    }
+    Context 'Escaped Content <Text>' {
+        It '(nyi) AutoLink invalid chars nested in Italic' -Skip {
+            # test cases from the specs: https://spec.commonmark.org/current
+        }
+    }
+}
