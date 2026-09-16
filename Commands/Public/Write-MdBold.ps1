918b9cff82dd0586780f0fe67876ad780238502e
Jake Bolton <ninmonkeys@gmail.com>
2026-09-16 11:58:36 -0500

    create: Write-MdBold and tests

---
+++ Commands/Public/Write-MdBold.ps1
@@ -0,0 +1,19 @@
+﻿function Write-MdBold {
+    <#
+    .SYNOPSIS
+        Writes Md Bold / strong text, like <strong>
+    .example
+        'file list:' | Write-MdBold
+        # **file list:**
+    #>
+    [OutputType( [string] )]
+    [CmdletBinding()]
+    param(
+        # Header text
+        [Parameter(ValueFromPipeline)]
+        [string] $Text
+    )
+    process {
+        '**{0}**' -f $Text
+    }
+}
---
+++ Tests/Commands/Public/Write-MdBold.Tests.ps1
@@ -0,0 +1,23 @@
+﻿Describe 'Write-MdBold' {
+    Context 'Bold: <Text>' -ForEach @(
+        @{
+            Text = 'File List:'
+            Expected = '**File List:**'
+            | NormalizeLineEnding
+        }
+    ) {
+        It 'By Param Name is: <Expected>' {
+            $actual = Write-MdBold -Text $Text | NormalizeLineEnding
+            $actual | Should-BeString -Expected $Expected # -TrimWhitespace # -NormalizeLineEnding
+        }
+        It 'By Param Pos is: <Expected>' {
+            $actual = Write-MdBold $Text | NormalizeLineEnding
+            $actual | Should-BeString -Expected $Expected # -TrimWhitespace # -NormalizeLineEnding
+        }
+    }
+    Context 'Escaped Content <Text>' {
+        It '(nyi) AutoLink invalid chars nested in bold' -Skip {
+            # test cases from the specs: https://spec.commonmark.org/current
+        }
+    }
+}
