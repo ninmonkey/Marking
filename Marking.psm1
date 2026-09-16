19b9a5b64e0b3894ccc9bb51caaaf3204ebe9689
Jake Bolton <ninmonkeys@gmail.com>
2026-09-16 12:44:59 -0500

    build: Marking v0.0.5
    New Commands:
    Write-MdCodeFence
    Write-MdCodeSpan
    Write-MdHorizontalRule
    Write-MdBold
    Write-MdItalic
    Write-MdLink
    
    other:
    New test cases for existing commands
    Urls escape spaces

--- Marking.psm1
+++ Marking.psm1
@@ -1,6 +1,6 @@
 <#
 .Description
-    Module built on: 2026-09-15 11:52:24Z
+    Module built on: 2026-09-16 12:38:10Z
 #>
 
 #region Module.Before.ps1
@@ -11,17 +11,130 @@
 
 #region Private Module Functions
 
+function Escape-LinkUrl {
+    <#
+    .SYNOPSIS
+        Escape markdown Url components
+    .description
+        - encode spaces as %20
+        - (nyi) escape unicode runes? check the specs.
+    #>
+    [CmdletBinding()]
+    param(
+        # Header text
+        [Parameter(ValueFromPipeline)]
+        [Alias('InputObject', 'Content')]
+        [string] $Text
+    )
+    begin {
+    }
+    process {
+        $Text -replace '[ ]', '%20'
+    }
+    end {
+
+    }
+}
+
 
 #endregion Private Module Functions
 
 
 #region Public Functions
 
+function Write-MdBold {
+    <#
+    .SYNOPSIS
+        Writes Md Bold / strong text, like <strong>
+    .example
+        'file list:' | Write-MdBold
+        # **file list:**
+    #>
+    [OutputType( [string] )]
+    [CmdletBinding()]
+    param(
+        # Header text
+        [Parameter(ValueFromPipeline)]
+        [string] $Text
+    )
+    process {
+        '**{0}**' -f $Text
+    }
+}
+
+function Write-MdCodeFence {
+    <#
+    .SYNOPSIS
+        Writes Md CodeFence
+    .example
+        Write-MdCodeFence $Code
+        $code | Write-MdCodeFence
+        $code | Write-MdCodeFence -Language 'json'
+    #>
+    [OutputType( [string] )]
+    [CmdletBinding()]
+    param(
+        # Code to wrap
+        [Alias('Content')]
+        [Parameter(ValueFromPipeline)]
+        [string[]] $Text,
+
+        # Language: c, js, py, etc. ( default: none )
+        [ArgumentCompletions('js', 'json', 'md', 'yml', 'ts', 'log', 'html', 'css', 'PowerShell')]
+        [string] $Language = '',
+
+        # Which character to use? ( Default: "---" )
+        [ValidateSet('`', '~')]
+        [string] $Character = '`'
+    )
+    begin {
+        $Ticks = $Character * 3 -join ''
+        $Prefix = '{0}{1}' -f $Ticks, $Language
+        $Suffix = '{0}' -f $Ticks
+        [Collections.Generic.List[object]] $Content = @()
+    }
+    process {
+        foreach( $Line in $Text ) {
+            $Content.Add( $Line )
+        }
+    }
+    end {
+        @(
+            "`n"
+            $Prefix
+            $Content | Join-String -sep "`n" -op "`n" -os "`n"
+            $Suffix
+            "`n"
+        ) -join ''
+    }
+}
+
+function Write-MdCodeSpan {
+    <#
+    .SYNOPSIS
+        Writes Md Code Span, like <code>
+    .example
+        'x = 0xff' | Write-MdCodeSpan
+        # <code>x = 0xff</code>
+    #>
+    [OutputType( [string] )]
+    [CmdletBinding()]
+    param(
+        # Code to wrap
+        [Parameter(ValueFromPipeline)]
+        [string] $Text
+    )
+    process {
+        '`{0}`' -f $Text
+    }
+}
+
 function Write-MdHeader {
     <#
     .SYNOPSIS
         Writes Md headers, like <h1>, <h2>, etc.
     #>
+    [OutputType( [string] )]
     [CmdletBinding()]
     param(
         # Header text
@@ -38,11 +151,99 @@ function Write-MdHeader {
     }
 }
 
+function Write-MdHorizontalRule {
+    <#
+    .SYNOPSIS
+        Writes Md Horizontal Rule like <hr>
+    .example
+        Write-MdHorizontalRule
+        # ----
+    #>
+    [OutputType( [string] )]
+    [CmdletBinding()]
+    param(
+        # Which character to use? ( Default: "---" )
+        [ValidateSet('-', '*', '_')]
+        [string] $Character = '-'
+    )
+    process {
+        $Render = $Character * 3 -join ''
+        "`n${Render}`n"
+    }
+}
+
+function Write-MdItalic {
+    <#
+    .SYNOPSIS
+        Writes Italic text
+    .example
+        'foo bar' | Write-MdItalic
+        # *foo bar*
+    #>
+    [OutputType( [string] )]
+    [CmdletBinding()]
+    param(
+        # Header text
+        [Parameter(ValueFromPipeline)]
+        [string] $Text
+    )
+    process {
+        '*{0}*' -f $Text
+    }
+}
+
+function Write-MdLink {
+    <#
+    .SYNOPSIS
+        Writes Md Links / Urls, like [Title](Url)
+    .example
+        Write-MdLink -Title 'Back to TOC' -Url '#toc'
+        # out: [Back to TOC](#toc)
+    #>
+    [OutputType( [string] )]
+    [CmdletBinding()]
+    param(
+        # Header text ( optional )
+        [Alias('Name')]
+        [object] $Title,
+
+        # Url to link to.
+        # Currently accepts any Url because sometimes you want invalid urls
+        [ArgumentCompletions(
+            "'#top'",
+            "'file:///c/some%20file.md'",
+            "'file.md'"
+        )]
+        [Parameter(Mandatory)]
+        [Alias('Link', 'Uri')]
+        [string] $Url
+    )
+    end {
+        # If title is missing, write <autolink.html> format
+        if( [string]::IsNullOrWhitespace( $Title )  ) {
+            return "<${Url}>"
+        }
+
+        $Url = $Url | Escape-LinkUrl
+        return '[{0}]({1})' -f $Title, $Url
+    }
+}
+
+# https://spec.commonmark.org/0.31.2/#link-reference-definitions
+
 function Write-MdList {
     <#
     .SYNOPSIS
         Writes Md lists, - item 1
+    .example
+        # show files
+        gci . -name | Write-MdList
+
+        # output:
+            # - readme.md
+            # - Marking.psd1
     #>
+    [OutputType( [string] )]
     [CmdletBinding()]
     param(
         # Header text
