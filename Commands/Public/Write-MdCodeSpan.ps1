6c03a7f315e3419e9f6eda69066de94fde6f795c
Jake Bolton <ninmonkeys@gmail.com>
2026-09-16 12:14:16 -0500

    create: Write-MdCodeSpan and tests

---
+++ Commands/Public/Write-MdCodeSpan.ps1
@@ -0,0 +1,19 @@
+﻿function Write-MdCodeSpan {
+    <#
+    .SYNOPSIS
+        Writes Md Code Span, like <code>
+    .example
+        'x = 0xff' | Write-MdCodeSpan
+        # <code>x = 0xff</code>
+    #>
+    [OutputType( [string] )]
+    [CmdletBinding()]
+    param(
+        # Code to wrap
+        [Parameter(ValueFromPipeline)]
+        [string] $Text
+    )
+    process {
+        '`{0}`' -f $Text
+    }
+}
---
+++ Tests/Commands/Public/Write-MdCodeSpan.Tests.ps1
@@ -0,0 +1,29 @@
+﻿Describe 'Write-MdCodeSpan' {
+    Context 'Span: <Text>' -ForEach @(
+        @{
+            Text = 'x = 0xff'
+            Expected = '`x = 0xff`'
+            | NormalizeLineEnding
+        }
+    ) {
+        It 'By Param Name is: <Expected>' {
+            $actual = Write-MdCodeSpan -Text $Text | NormalizeLineEnding
+            $actual | Should-BeString -Expected $Expected # -TrimWhitespace # -NormalizeLineEnding
+        }
+        It 'By Param Pos is: <Expected>' {
+            $actual = Write-MdCodeSpan $Text | NormalizeLineEnding
+            $actual | Should-BeString -Expected $Expected # -TrimWhitespace # -NormalizeLineEnding
+        }
+        It 'By Pipeline is: <Expected>' {
+            $Text
+                | Write-MdCodeSpan
+                | NormalizeLineEnding
+                | Should-BeString -Expected $Expected # -TrimWhitespace # -NormalizeLineEnding
+        }
+    }
+    Context 'Escaped Content <Text>' {
+        It '(nyi) Auto escape nested backticks etc in codepan' -Skip {
+            # test cases from the specs: https://spec.commonmark.org/current
+        }
+    }
+}
